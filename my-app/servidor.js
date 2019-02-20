2a9f417e6910277f4898f9488dc7873b0a65cc37
Jean42Medina <40485748+Jean42Medina@users.noreply.github.com>
2019-02-19 23:13:09 -0500

    Add files via upload

---
+++ my-app/servidor.js
@@ -0,0 +1,13 @@
+// ejercicio de cursividad
+
+function factorial (numero){
+    let resultado = 1;
+    for (let cuenta = 1; cuenta<=numero; cuenta++){
+        resultado *= cuenta;
+
+    }
+    return resultado
+}
+    letf5 = factorial (5);
+    letf4 = factorial (4);
+    console.lof(f5);
